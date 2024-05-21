d5848c2a6ac9e5d4aeccf616f6fab5e14b172804
andrii_h <galgayand@gmail.com>
2024-05-21 14:11:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './App.css'
-// import Layout from "./components/Layout/Layout.tsx";
+import Layout from "./components/Layout/Layout.tsx";
 import {useEffect} from "react";
 import {useAppDispatch, useAppSelector} from "./hooks/hooks.ts";
-// import BoardsList from "./components/common/BoardsList/BoardsList.tsx";
+import BoardsList from "./components/common/BoardsList/BoardsList.tsx";
 import {useLazyGetAllBoardsQuery} from "./services/board.ts";
-// import {Box} from "@mui/material";
+import {Box} from "@mui/material";
 import {boardAction} from "./store/board/boardSlice.ts";
-// import CreateBoard from "./components/Header/CreateBoard/CreateBoard.tsx";
+import CreateBoard from "./components/Header/CreateBoard/CreateBoard.tsx";
 import {categoryAction} from "./store/category/categorySlice.ts";
 import {taskAction} from "./store/task/taskSlice.ts";
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
   const [getAllBoards] = useLazyGetAllBoardsQuery()
   const dispatch = useAppDispatch()
-  const [count, setCount] = useState(0)
 
   const {currentBoard, boardsList} = useAppSelector(
     state => state.board
@@ -36,26 +31,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!boardsList && <CreateBoard/>}
+      {boardsList && !currentBoard &&
+        <Box maxHeight={'80vh'} overflow={"auto"}>
+          <CreateBoard/>
+          <BoardsList/>
+        </Box>
+      }
+      {!boardsList || currentBoard && <Layout/>}
     </>
   )
 }
