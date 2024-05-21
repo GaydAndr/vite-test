bb1c040263232b95ae7da4620ec1bb3bf5d4e418
andrii_h <galgayand@gmail.com>
2024-05-21 13:52:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {theme} from "./styles/styles.ts";
+import {ThemeProvider} from "@mui/material";
+import {Provider} from "react-redux";
+import {persistor, store} from "./store/store.ts";
+import {PersistGate} from "redux-persist/integration/react";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <App/>
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>,
 )
