781dc93a347776c527068237be6600ac02428959
andrii_h <galgayand@gmail.com>
2024-05-21 14:06:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import './App.css'
+import Layout from "./components/Layout/Layout.tsx";
+import {useEffect} from "react";
+import {useAppDispatch, useAppSelector} from "./hooks/hooks.ts";
+import BoardsList from "./components/common/BoardsList/BoardsList.tsx";
+import {useLazyGetAllBoardsQuery} from "./services/board.ts";
+import {Box} from "@mui/material";
+import {boardAction} from "./store/board/boardSlice.ts";
+import CreateBoard from "./components/Header/CreateBoard/CreateBoard.tsx";
+import {categoryAction} from "./store/category/categorySlice.ts";
+import {taskAction} from "./store/task/taskSlice.ts";
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
+  const [getAllBoards] = useLazyGetAllBoardsQuery()
+  const dispatch = useAppDispatch()
   const [count, setCount] = useState(0)
+
+  const {currentBoard, boardsList} = useAppSelector(
+    state => state.board
+  )
+  useEffect(() => {
+    if (!currentBoard && !boardsList) {
+      getAllBoards()
+    }
+    if (!currentBoard && boardsList?.length) {
+      dispatch(boardAction.setBoard(boardsList[0]))
+      dispatch(categoryAction.setCategories(boardsList[0].sub_list))
+      dispatch(taskAction.setTask(boardsList[0].tasks_list))
+    }
+  }, [boardsList])
+
 
   return (
     <>
